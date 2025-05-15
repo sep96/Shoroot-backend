48616a57fe6d9869fc7e927eea06c811638b673e
James <sep7696@gmail.com>
2025-05-15 14:30:27 +0330

    feat : setup main.rs with module declarations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+mod db;
+mod auth;
+mod bets;
+mod users;
+mod models;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting server on http://127.0.0.1:8080");
+    // TODO: Load config, connect db, mount routes
+    Ok(())
 }
