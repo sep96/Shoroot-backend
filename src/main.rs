3c7548606000981e2c878000fd76f74780744ddf
James <sep7696@gmail.com>
2025-05-15 14:34:05 +0330

    feat : Setup server with user registration route

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-mod config;
-mod db;
-mod auth;
-mod bets;
-mod users;
-mod models;
+use actix_web::{App, HttpServer, web};
+use config::Config;
+use db::connect;
+use auth::handler::register;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting server on http://127.0.0.1:8080");
-    // TODO: Load config, connect db, mount routes
-    Ok(())
+    dotenv::dotenv().ok();
+    let config = Config::from_env();
+    let pool = connect(&config.database_url).await;
+
+    println!("Server running at http://127.0.0.1:8080");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .service(register)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
